4bd7a9742076c98b7101fed2d47828ec27ab5de9
Brynjar Fredriksen <bafbomb@gmail.com>
2017-11-15 17:02:19 -0800

    moar

--- scripts/install_mongo.sh
+++ scripts/install_mongo.sh
@@ -30,10 +30,10 @@ EOF
      sudo   systemctl enable mongodb
 
     else
-        apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-        echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
-        apt-get update
-        apt-get install -y mongodb-org
+       sudo  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+       sudo  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
+       sudo apt-get update
+       sudo  apt-get install -y mongodb-org
     fi
 
 elif [ -f /etc/redhat-release ]; then
